a305d45d68117dee75d279141ba4f7f770f45bfc
Jacob Stoebel <jstoebel@gmail.com>
2017-04-04 16:31:44 -0400

    add generator
    update docs to match

---
+++ lib/generators/orphanage/init_generator.rb
@@ -0,0 +1,79 @@
+require 'rails/generators/base'
+
+module Orphanage
+
+  class InitGenerator < Rails::Generators::Base
+    containing_dir = File.expand_path("..", __FILE__)
+    template_dir = File.join(containing_dir, 'templates')
+    source_root File.expand_path(template_dir)
+
+    argument :home_model_name, :type => :string
+
+    def ensure_home_model_and_table_exists
+
+      begin
+        home_model
+      rescue NameError => e
+        fail "Home model not found. Plese generate it first."
+      end
+
+      # presumed_model_path = "#{::Rails.root}/app/models/#{home_file_name}.rb"
+      # model_exists = FileTest.exist? presumed_model_path
+      # puts "WARNING: model #{home_model_name} not found at #{presumed_model_path}" if !model_exists
+
+      table_exists = ActiveRecord::Base.connection.data_source_exists? home_table_name
+      fail "table #{home_table_name} does not exist. Stopping." if !table_exists
+
+    end # ensure_home_model_exists
+
+    def generate_model
+      generate "model #{home_model_name}Temp --skip-migration"
+
+    end
+
+    def orphanize_model
+      # change the model to be an orphan model
+      # gsub_file "app/models/#{file_name}/.rb", "replaced!"
+      puts "replacing model..."
+      template "model.rb.erb", "app/models/#{home_file_name}_temp.rb", force: true
+    end
+
+    def create_migration
+      puts "creating migration file..."
+
+      migration_file_name = "#{Time.now.utc.strftime("%Y%m%d%H%M%S")}_create_#{home_file_name}_temps.rb"
+      template "migration.rb.erb", "db/migrate/#{migration_file_name}"
+
+    end # create_migration
+
+    private
+
+    def home_model
+      home_model_name.constantize
+    end # home_model
+
+    def home_file_name
+      home_model_name.underscore
+    end # file_name
+
+    def home_table_name
+      home_file_name.pluralize
+    end # home_table_name
+
+    def orphan_table_name
+      (home_model_name + "Temp").underscore.pluralize
+    end # orphan_table_name
+
+    def migration_columns
+      # get all column names
+      needed_cols = home_model.columns.select { |col| !%w(id created_at updated_at).include? col.name }
+
+      # returns an array of strings each representing a single column to add in the migration.
+      migrations = needed_cols.map {|col| "t.#{col.sql_type_metadata.type} :#{col.name}, null: #{col.null}" }
+
+      return migrations
+
+    end # orphan_columns
+  end
+
+end
